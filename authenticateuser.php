35d0f4d83b3eb9e60dd257dea723218b98cca7cd
ruiyang00 <reyyang00@gmail.com>
2020-03-07 21:40:14 -0800

    ..

--- authenticateuser.php
+++ authenticateuser.php
@@ -20,22 +20,8 @@ if (isset($_POST['submit_form'])) {
         }
         $_SESSION[user_ID] = $arr[0];
         $_SESSION[user_Name] = $arr[1];
+        header('Location:index.php');
 
-        echo '<p class = "login-status"> You are logging, below is our company secret clients</p>';
-
-        $contacts = file('secret.txt');
-
-        echo '<table>';
-        foreach ($contacts as $info) {
-            echo '<tr>';
-            echo "<td>{$info}</td>";
-            echo '</tr>';
-        }
-        echo '</table>';
-
-    } else {
-
-        echo '<p class = "login-status"> Welcome to my cmpe272 Athena project, a distributed key-value software, main page</p>';
     }
 
 }
--- index.php
+++ index.php
@@ -6,8 +6,24 @@ require "header.php"
 <main>
 
     <?php
+if (isset($_SESSION['user_Name'])) {
+    echo '<p class = "login-status"> You are logging, below is our company secret clients</p>';
+    
+    $contacts = file('secret.txt');
+
+    echo '<table>';
+    foreach ($contacts as $info) {
+        echo '<tr>';
+        echo "<td>{$info}</td>";
+        // echo "<td>{$var['category'][$i]}</td>";
+        echo '</tr>';
+    }
+    echo '</table>';
+    
+} else {
 
     echo '<p class = "login-status"> Welcome to my cmpe272 Athena project, a distributed key-value software, main page</p>';
+}
 ?>
 </main>
 
