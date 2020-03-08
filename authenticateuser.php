6682d9560aed711ec19fbe08595de45e50c77064
ruiyang00 <reyyang00@gmail.com>
2020-03-07 21:55:44 -0800

    ..

--- authenticateuser.php
+++ authenticateuser.php
@@ -5,24 +5,24 @@ require "header.php"
 <main>
 
     <?php
-if (isset($_POST['submit_form'])) {
-    $file = "users.txt";
-    $content = file_get_contents($file);
-    $arr = explode(" ", $content);
+    if (isset($_POST['submit_form'])) {
+        $file = "users.txt";
+        $content = file_get_contents($file);
+        $arr = explode(" ", $content);
 
-    $uname = $_POST['uname'];
-    $psw = $_POST['psw'];
+        $uname = $_POST['uname'];
+        $psw = $_POST['psw'];
 
-    if ($arr[0] == $uname && $arr[1] == $psw) {
+        if ($arr[0] == $uname && $arr[1] == $psw) {
 
-        if (!isset($_SESSION)) {
-            session_start();
+            if (!isset($_SESSION)) {
+                session_start();
+            }
+            $_SESSION[user_ID] = $arr[0];
+            $_SESSION[user_Name] = $arr[1];
+            header('Location:index.php');
+
         }
-        $_SESSION[user_ID] = $arr[0];
-        $_SESSION[user_Name] = $arr[1];
-        header('Location:index.php');
-
-    }
 
 }
 
