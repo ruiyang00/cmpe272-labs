e750de0211022734d05d7be572ec1df7c3c7613e
ruiyang00 <reyyang00@gmail.com>
2020-04-18 22:49:10 -0700

    lab for curl

--- users.php
+++ users.php
@@ -1,22 +1,22 @@
 <?php
 require "header.php";
 
-$hn = "localhost";
-$un = "cmpe272user";
-$pw = "cmpe272af!";
-$db = "cmpe272";
-$conn = new mysqli($hn, $un, $pw, $db);
-// $query = "SELECT * FROM users";
-$query = "SELECT * FROM users WHERE $searchbytype ='$input' ";
-$db_query = mysqli_query($conn, $query);
-$result = mysqli_fetch_assoc($db_query);
-$conn->close();
 ?>
 
 <body>
 
     <p>
     <?php
+        $hn = "localhost";
+        $un = "cmpe272user";
+        $pw = "cmpe272af!";
+        $db = "cmpe272";
+        $conn = new mysqli($hn, $un, $pw, $db);
+        // $query = "SELECT * FROM users";
+        $query = "SELECT * FROM users WHERE $searchbytype ='$input' ";
+        $db_query = mysqli_query($conn, $query);
+        $result = mysqli_fetch_assoc($db_query);
+        $conn->close();
         echo '<table>';
         foreach ($result as $user) {
             echo '<tr>';
