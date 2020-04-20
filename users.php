30b70a34429cb1a017a57fa6651fec317938b688
ruiyang00 <reyyang00@gmail.com>
2020-04-19 18:47:38 -0700

    lab for curl

--- users.php
+++ users.php
@@ -5,6 +5,10 @@ require "header.php";
 
 <body>
 
+<h1>
+    Belos are the users from this own company's database ruiyang90.info
+</h1>
+
     <table>
         <tr>
             <td class="tableheader"><strong>First Name</strong></td>
@@ -15,42 +19,41 @@ require "header.php";
             <td class="tableheader"><strong>Cell Phone</strong></td>
         </td>
     <?php
-        $hn = "localhost";
-        $un = "cmpe272user";
-        $pw = "cmpe272af!";
-        $db = "cmpe272";
-        $conn = new mysqli($hn, $un, $pw, $db);
-        // $query = "SELECT * FROM users";
-        $query = "SELECT * FROM users";
-        $query_res = mysqli_query($conn, $query);
-        
+$hn = "localhost";
+$un = "cmpe272user";
+$pw = "cmpe272af!";
+$db = "cmpe272";
+$conn = new mysqli($hn, $un, $pw, $db);
+// $query = "SELECT * FROM users";
+$query = "SELECT * FROM users";
+$query_res = mysqli_query($conn, $query);
 
-        // print_r($query_res);
-        
-        while($user = $query_res -> fetch_assoc()){
-            echo "<tr>";
-                echo "<td>";
-                    echo $user["fn"];
-                echo "</td>";
-                echo "<td>";
-                    echo $user["ln"];
-                echo "</td>";
-                echo "<td>";
-                    echo $user["email"];
-                echo "</td>";
-                echo "<td>";
-                    echo $user["haddress"];
-                echo "</td>";
-                echo "<td>";
-                    echo $user["hphone"];
-                echo "</td>";
-                echo "<td>";
-                    echo $user["cphone"];
-                echo "</td>";
-            echo "</tr>";
-    
-        }
-    ?>
+// print_r($query_res);
+
+while ($user = $query_res->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>";
+    echo $user["fn"];
+    echo "</td>";
+    echo "<td>";
+    echo $user["ln"];
+    echo "</td>";
+    echo "<td>";
+    echo $user["email"];
+    echo "</td>";
+    echo "<td>";
+    echo $user["haddress"];
+    echo "</td>";
+    echo "<td>";
+    echo $user["hphone"];
+    echo "</td>";
+    echo "<td>";
+    echo $user["cphone"];
+    echo "</td>";
+    echo "</tr>";
+
+}
+?>
     </table>
 <body>
 
